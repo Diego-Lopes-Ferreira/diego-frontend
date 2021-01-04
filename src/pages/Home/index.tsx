0b2a6a4660281712d37d9ba09f7ced52ece918f8
Diego-Lopes-Ferreira <diego.lopes.ferreira.br@gmail.com>
2021-01-04 11:54:25 -0300

    feat: started pages structure + started main page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import About from './components/About'
+
+const Home: React.FC = () => {
+  return (
+    <div className="home">
+      <About />
+    </div>
+  );
+}
+
+export default Home;
